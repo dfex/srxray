26bad41bdeb28a243b93b9b5b06e09ed304e082f
Ben Dale <ben.dale@gmail.com>
2019-01-04 01:15:03 +1000

    First crack at flattenJunosConfig

--- srxray.py
+++ srxray.py
@@ -4,28 +4,68 @@ import openpyxl
 import logging
 import argparse
 
-def flattenJunosConfig(junosConfig):
-	"""Take a Junos configuration file in Junos (bracketed) format and flatten it into set format"""
-	pass
+def flattenJunosConfig(JunosConfig):
+	"""Take a Junos configuration file in bracketed format and flattens it into set format"""
+	JunosSetConfig=[]
+	JunosSetLine=['set ']
+	for line in JunosConfig:
+			if (line.endswith("{")):
+				if (line.lstrip().startswith("protect:")):
+					JunosSetLine[0]="protect "
+					JunosSetLine.append(line.rstrip("{").lstrip().replace('protect: ','',1)) # Remove protect from within command without stripping
+					JunosSetConfig.append(JunosSetLine[:]) # Copy by value, not reference
+					JunosSetLine[0]="set "
+				else:										
+					JunosSetLine.append(line.rstrip("{").lstrip())
+			elif (line.endswith(";")):
+				if (line.lstrip().startswith("protect:")):
+					# First create the line as normal without the protect statement:
+					JunosSetLine.append(line.rstrip(";").lstrip(" ").replace('protect: ','',1)) # Remove protect from within command without stripping
+					JunosSetConfig.append(JunosSetLine[:]) # Copy by value, not reference
+					JunosSetLine.pop()
+					# Now create the protected line without the parameter on the end:
+					JunosSetLine[0]="protect "
+					logging.debug(JunosSetLine)
+					JunosSetLine.append(" ".join(line.rstrip(";").lstrip(" ").replace('protect: ','',1).split()[:2])) # Removes protect and only appends first 2 config elements - UGLY
+					JunosSetConfig.append(JunosSetLine[:]) # Copy by value, not reference
+					JunosSetLine[0]="set "
+					JunosSetLine.pop()
+				else:
+					JunosSetLine.append(line.rstrip(";").lstrip())
+					JunosSetConfig.append(JunosSetLine[:]) # Copy by value, not reference
+					JunosSetLine.pop()
+			elif (line.endswith("}")):
+				JunosSetLine.pop()
+	return(JunosSetConfig)
 
 def loadJunosConfig(pathToConfig):
-	"""Open a Junos configuration file - if the file is in set format, return it, otherwise convert it to set format
-	and return it"""
-	JunosConfigFile = open(pathToConfig, "r")
-	firstLine = JunosConfigFile.readline()
-	JunosConfigFile.seek(0)
-	if (firstLine.startswith("set version")):
-		logging.info("File is in set format")
-	elif (firstLine.startswith("## Last commit:") | firstLine.startswith("version")):
-		logging.info("File is in JCONF format")
-	else:
-		logging.info("File does not appear to be a valid Junos configuration")
-	JunosConfigFile.close()
+	"""Open a Junos configuration file - if the file is in set format, return it as a list, otherwise
+	convert it to set format and then return it as a list and return it"""
+	JunosConfig=[]
+	with open(pathToConfig, "r") as JunosConfigFile:
+		for line in JunosConfigFile:
+			JunosConfig.append(line.rstrip())
+		if (JunosConfig[0].startswith("set version")):
+			logging.info("File is in set format")
+			return JunosConfig
+		elif (JunosConfig[0].startswith("## Last commit:") | JunosConfig[0].startswith("version")):
+			logging.info("File is in JCONF format")
+			return (flattenJunosConfig(JunosConfig))
+		else:
+			logging.info("File does not appear to be a valid Junos configuration")
+
+def printJunosConfig(JunosConfig):
+	for line in JunosConfig:
+		for term in line:
+			print (term, end="")
+		print ()
+
+
 
 def main():
-	loadJunosConfig("0ffnet-srx210-gw.conf")
-	loadJunosConfig("0ffnet-srx210-gw-set.conf")
-	loadJunosConfig("randomcrap.conf")
+	printJunosConfig(loadJunosConfig("justgroups-set.conf"))
+	print("###")
+	printJunosConfig(loadJunosConfig("0ffnet-srx210-gw.conf"))
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
