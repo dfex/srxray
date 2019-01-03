21fdfb57dfc09b87c1c9303e07c45806110309d8
Ben Dale <ben.dale@gmail.com>
2019-01-03 22:35:31 +1000

    Addeded logging foundation

--- srxray.py
+++ srxray.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import openpyxl
+import logging
+import argparse
 
 def flattenJunosConfig(junosConfig):
 	"""Take a Junos configuration file in Junos (bracketed) format and flatten it into set format"""
@@ -13,13 +15,21 @@ def loadJunosConfig(pathToConfig):
 	firstLine = JunosConfigFile.readline()
 	JunosConfigFile.seek(0)
 	if (firstLine.startswith("set version")):
-		print("File is in set format")
+		logging.info("File is in set format")
 	elif (firstLine.startswith("## Last commit:") | firstLine.startswith("version")):
-		print("File is in JCONF format")
+		logging.info("File is in JCONF format")
 	else:
-		print ("File does not appear to be a valid Junos configuration")
+		logging.info("File does not appear to be a valid Junos configuration")
 	JunosConfigFile.close()
 
-loadJunosConfig("0ffnet-srx210-gw.conf")
-loadJunosConfig("0ffnet-srx210-gw-set.conf")
-loadJunosConfig("randomcrap.conf")
+def main():
+	loadJunosConfig("0ffnet-srx210-gw.conf")
+	loadJunosConfig("0ffnet-srx210-gw-set.conf")
+	loadJunosConfig("randomcrap.conf")
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--log', choices=['INFO', 'DEBUG', 'WARN'], help="Debug logging level")
+	args = parser.parse_args()
+	logging.basicConfig(level=args.log)
+	main()
